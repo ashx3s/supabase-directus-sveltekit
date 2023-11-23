85e8b1c3515ede3c9cf1bf07d3a7dc5ea74ec7a9
Ashlyn Knox <ashlyn@lilyx.io>
2023-11-23 16:47:29 -0700

    render supabase and store data
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,12 @@
 <script>
 	import TodoView from '$lib/components/TodoView.svelte';
-	import { todos } from '$lib/stores.js';
-	let todoList;
-
-	todos.subscribe((value) => {
-		todoList = value;
-	});
-	console.log(todoList);
+	export let data;
+	let { todos } = data;
+	console.log(todos);
 </script>
 
 <header class="bg-blue-200 h-1/5 grid place-items-center">
 	<h1 class="text-4xl">TODO App</h1>
 </header>
 
-<TodoView todos={todoList} />
+<TodoView supabaseTodos={todos} />

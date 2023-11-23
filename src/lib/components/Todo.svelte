a991cd37fa830d7688b1b52c535b241f5363befd
Ashlyn Knox <ashlyn@lilyx.io>
2023-11-23 16:42:25 -0700

    component edits for stores and supabase
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

---
+++ src/lib/components/Todo.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Icon from '@iconify/svelte';
+
+	export let todo;
+	let { id, text } = todo;
+</script>
+
+<div class="flex gap-13 bg-pink-100 px-4 py-2 rounded-md my-2 w-fit">
+	<p><span class="text-xl font-bold">{id}: </span>{text}</p>
+	<button
+		class="hover:text-gray-601 px-2 rounded-full active:bg-pink-300 hover:bg-pink-200 transition duration-150 ease-linear"
+		on:click><Icon icon="fa6-solid:xmark" /></button
+	>
+</div>
--- src/lib/components/TodoView.svelte
+++ src/lib/components/TodoView.svelte
@@ -1,12 +1,32 @@
 <script>
-	export let todos = [{ text: 'a' }];
+	import { createTodos } from '$lib/stores.js';
+	import Todo from './Todo.svelte';
+	export let supabaseTodos = [];
+	let uid = 1;
+	let defaultTodos = [
+		{ id: uid++, text: 'a task', done: false },
+		{ id: uid++, text: 'second task', done: false },
+		{ id: uid++, text: 'third task', done: false },
+		{ id: uid++, text: 'fourth task', done: false }
+	];
+	const todos = createTodos(defaultTodos);
+	console.log($todos);
 </script>
 
 <section>
-	<h2>Current Tasks</h2>
+	<h2 class="text-2xl">Supabase Tasks</h2>
 	<ul>
-		{#each todos as todo}
-			<li>{todo.text}</li>
+		{#each supabaseTodos as todo}
+			<p>{todo.text}</p>
+		{/each}
+	</ul>
+</section>
+
+<section class="ml-4 mt-8">
+	<h2 class="text-2xl">Stores Current Tasks</h2>
+	<ul>
+		{#each $todos as todo}
+			<Todo {todo} on:click={() => todos.remove(todo)} />
 		{/each}
 	</ul>
 </section>

afc76ad3d8171a277a6b1157e4f244756b13e99c
Ashlyn Knox <ashlyn@lilyx.io>
2023-11-23 07:53:32 -0700

    tailwind setup
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,11 @@
+<header>
+	<div>LOGO</div>
+	<nav>
+		<ul>
+			<li><a href="/">route</a></li>
+			<li><a href="/">route</a></li>
+			<li><a href="/">route</a></li>
+		</ul>
+	</nav>
+	<div>icons</div>
+</header>
